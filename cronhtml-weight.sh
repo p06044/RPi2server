bbc5cb5fc547a3da29fba3da97bc9177558a0998
sortme tetsuya <sortme@sortme-no-MacBook-Pro.local>
2017-07-01 22:47:48 +0900

    change image size

--- cronhtml-weight.sh
+++ cronhtml-weight.sh
@@ -10,16 +10,19 @@ echo "<html>
         setTimeout(\"location.reload()\",1000*60);
     </SCRIPT>" >> /home/pi/cron.html
 
+#日付パート
 echo `date` >> /home/pi/cron.html
 
+#SAMBAパート
 ls -tl share | tail -n +2 | awk '{print $6,$7,$8,$9}' | while read line
 do
-        echo "<div>${line}</div>" >> /home/pi/cron.html
-
+	echo "<div>${line}</div>" >> /home/pi/cron.html
 done
 
-echo "<div><img src=\"weight.png\" width=\"50%\"></div>" >> /home/pi/cron.html
+#体重パート
+echo "<div><img src=\"weight.png\" width=\"30%\"></div>" >> /home/pi/cron.html
 
+#CRONパート
 crontab -l | while read line
 do
         echo "<div>${line}</div>" >> /home/pi/cron.html
