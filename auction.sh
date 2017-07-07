87966b1265d108ab4f12fc7382948b8d12f15e20
p06044 <p06044@gmail.com>
2017-07-08 03:47:37 +0900

    fix search word

--- auction.sh
+++ auction.sh
@@ -19,7 +19,7 @@ RET='\
 '
 
 #メルカリパート
-wget -O auclist.html "https://www.mercari.com/jp/search/?sort_order=&keyword=%E8%A6%81%E9%A0%85%E9%9B%86&category_root=&brand_name=&brand_id=&size_group=&price_min=&price_max=&status_on_sale=1" &
+wget -O auclist.html "https://www.mercari.com/jp/search/?sort_order=&keyword=kals+%E8%A6%81%E9%A0%85%E9%9B%86&category_root=&brand_name=&brand_id=&size_group=&price_min=&price_max=&status_on_sale=1" &
 wait $!
 NAMES=($(cat auclist.html | grep items-box-name | sed -e "s/^.*<h3 class=\"items-box-name font-2\">\(.*\)<\/h3>.*$/\1/" | sed -e "s/　//g" -e "s/ //g"))
 PRICES=($(cat auclist.html | grep items-box-price | sed -e "s/^.*<div class=\"items-box-price font-5\">\(.*\)<\/div>.*$/\1/" | sed -e "s/¥ //g" -e "s/,//g"))
