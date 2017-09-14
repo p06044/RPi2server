257f36fa2aa75bc6308bae0f79c86adb0bd9eeb7
p06044 <p06044@gmail.com>
2017-09-14 22:31:53 +0900

    fix

--- cronhtml.sh
+++ cronhtml.sh
@@ -15,14 +15,15 @@ echo `date` >> /home/pi/cron.html
 
 #ページリンクパート
 echo "<a href=\"amazonlist.html\">[ama]</a><span style=\"color:orange;\">[cron]</span><a href=\"auctionlist.html\">[meru]</a><a href=\"ya.html\">[yauc]</a>" >> /home/pi/cron.html
+
+#センターカウントダウン
+bash /home/pi/timediff.sh
+
 #SAMBAパート
 ls -tl share | tail -n +2 | awk '{print $6,$7,$8,$9}' | while read line
 do
 	echo "<div>${line}</div>" >> /home/pi/cron.html
 done
-
-#センターカウントダウン
-bash /home/pi/timediff.sh
 
 #体重パート
 echo "<div><img src=\"weight.png\" width=\"30%\"></div>" >> /home/pi/cron.html
