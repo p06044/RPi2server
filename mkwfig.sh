89fcde3f935f45c9e7f909252af967cd410b21a1
p06044 <p06044@gmail.com>
2017-09-17 13:05:34 +0900

    fix

--- mkwfig.sh
+++ mkwfig.sh
@@ -4,7 +4,4 @@ A=/var/www/html/weight.png
 B=/home/pi/share/統計記録.xls
 if [ $A -ot $B ]; then
 	python weight.py
-else
-	echo `date` > ${HOME}/share/weight.log
-	echo "no update" >> ${HOME}/share/weight.log
 fi
