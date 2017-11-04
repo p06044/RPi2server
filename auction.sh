4339eaa49b6305ad2479b150ac06d25d99a69dc5
p06044 <p06044@gmail.com>
2017-11-05 02:05:57 +0900

    miedai

--- auction.sh
+++ auction.sh
@@ -31,7 +31,21 @@ echo "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.
 </body>
 </html>" >> /home/pi/auctionlist.html
 
-#メルカリパート
+#メルカリパート三重大学
+wget -O auclist.html "https://www.mercari.com/jp/search/?sort_order=price_asc&keyword=%E4%B8%89%E9%87%8D%E5%A4%A7%E5%AD%A6&category_root=5&category_child=72&category_grand_child%5B1124%5D=1&brand_name=&brand_id=&size_group=&price_min=&price_max=&status_on_sale=1" &
+wait $!
+NAMES=($(cat auclist.html | grep items-box-name | sed -e "s/^.*<h3 class=\"items-box-name font-2\">\(.*\)<\/h3>.*$/\1/" | sed -e "s/　//g" -e "s/ //g"))
+PRICES=($(cat auclist.html | grep items-box-price | sed -e "s/^.*<div class=\"items-box-price font-5\">\(.*\)<\/div>.*$/\1/" | sed -e "s/¥ //g" -e "s/,//g"))
+LINK=($(cat auclist.html | grep -B1 "<figure class=\"items-box-photo\">" | grep href | sed -e "s/^.*<a href=\"\(.*\)\">.*$/\1/"))
+i=0
+j=`expr ${#NAMES[@]}`
+while [ $i -ne $j ]
+do
+	echo "<div><span style=\"color:black;\">${NAMES[$i]}</span><a href=\"${LINK[$i]}\" target=\"_blank\">${PRICES[$i]}</a></div>" >> /home/pi/auctionlist.html
+	i=`expr $i + 1`
+done
+
+#メルカリパート要項集
 wget -O auclist.html "https://www.mercari.com/jp/search/?sort_order=&keyword=kals+%E8%A6%81%E9%A0%85%E9%9B%86&category_root=&brand_name=&brand_id=&size_group=&price_min=&price_max=&status_on_sale=1" &
 wait $!
 NAMES=($(cat auclist.html | grep items-box-name | sed -e "s/^.*<h3 class=\"items-box-name font-2\">\(.*\)<\/h3>.*$/\1/" | sed -e "s/　//g" -e "s/ //g"))
--- ya.sh
+++ ya.sh
@@ -34,7 +34,21 @@ echo "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.
 #ヤフオクリンク
 echo "<a href=\"https://auctions.yahoo.co.jp/search/search?p=kals+%E8%A6%81%E9%A0%85%E9%9B%86&auccat=0&fixed=0\" target=\"_blank\">ヤフオク</a>" >> /home/pi/ya.html
 
-#ヤフオクパート
+#ヤフオクパート三重大
+wget -O yalist.html "https://auctions.yahoo.co.jp/rss?p=%E4%B8%89%E9%87%8D%E5%A4%A7%E5%AD%A6&ei=UTF-8&oq=&auccat=2084008618&slider=0&fixed=0&s1=cbids&o1=a" &
+wait $!
+NAMES=($(cat yalist.html| sed -e "/ヤフオク/d" | grep -A1 title | grep title | sed -e "s/^.*<title>\(.*\)<\/title>.*$/\1/" -e "s/ //g" -e "s/　//g"))
+PRICES=($(cat yalist.html| grep 現在価格 | sed -e "s/^.*現在価格:\(.*\)円.*$/\1/" -e "s/,//g"))
+LINK=($(cat yalist.html | sed "/\/\/auctions/d" | grep \<link\> | sed -e "s/^.*<link>\(.*\)<\/link>.*$/\1/"))
+i=0
+j=`expr ${#NAMES[@]}`
+while [ $i -ne $j ]
+do
+	echo "<div><span style=\"color:black;\">${NAMES[$i]}</span><a href=\"${LINK[$i]}\" target=\"_blank\">${PRICES[$i]}</a></div>" >> /home/pi/ya.html
+	i=`expr $i + 1`
+done
+
+#ヤフオクパート要項集
 wget -O yalist.html "https://auctions.yahoo.co.jp/rss?p=kals+%E8%A6%81%E9%A0%85%E9%9B%86&oq=&auccat=0&fixed=0&ei=UTF8" &
 wait $!
 NAMES=($(cat yalist.html| sed -e "/ヤフオク/d" | grep -A1 title | grep title | sed -e "s/^.*<title>\(.*\)<\/title>.*$/\1/" -e "s/ //g" -e "s/　//g"))
