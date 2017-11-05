72932ae6eca57c03b9cf9ce1db8ddfce4ecd3df8
p06044 <p06044@gmail.com>
2017-11-06 00:44:32 +0900

    enhance color mie other year

--- auction.sh
+++ auction.sh
@@ -44,6 +44,8 @@ do
 	if [ `echo ${NAMES[$i]} | grep '2015' | grep '三重' ` ] ; then
 	
 		echo "<div><span style=\"color:red;\">${NAMES[$i]}</span><a href=\"${LINK[$i]}\" target=\"_blank\">${PRICES[$i]}</a></div>" >> /home/pi/auctionlist.html
+	elif [ `echo ${NAMES[$i]} | grep '三重' | grep -e '2014' -e '2013' -e '2016' -e '2017' ` ] ; then
+		echo "<div><span style=\"color:orange;\">${NAMES[$i]}</span><a href=\"${LINK[$i]}\" target=\"_blank\">${PRICES[$i]}</a></div>" >> /home/pi/auctionlist.html
 	else
 		echo "<div><span style=\"color:black;\">${NAMES[$i]}</span><a href=\"${LINK[$i]}\" target=\"_blank\">${PRICES[$i]}</a></div>" >> /home/pi/auctionlist.html
 	fi
