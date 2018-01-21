303556ffbb3a1ced467816cc465a0554993a3480
p06044 <p06044@gmail.com>
2018-01-21 14:03:22 +0900

    update

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+git init &
+wait $!
+git remote add origin https://github.com/p06044/RPi2server.git &
+wait $!
+git fetch &
+wait $!
+git merge origin/master &
+wait $!
+#login information
+i=1
+cat $HOME/login.txt | while read line
+do
+        if [ $i -eq 1 ]; then
+                n=${line}
+        elif [ $i -eq 2 ]; then
+                m=${line}
+        fi
+        i=`expr $i + 1`
+        if [ $i -eq 3 ]; then
+		git config --global user.name $n
+		git config --global user.email $m
+        fi
+done
+
+cp index.html /var/www/html/
+cp phpbutton.* /var/www/html/
+cp timer.html /var/www/html/
+crontab crontab.txt 
