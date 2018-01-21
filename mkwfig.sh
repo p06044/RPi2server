196c28e1b04596b7361d9df2e9abe82e6b791369
p06044 <p06044@gmail.com>
2018-01-21 16:10:04 +0900

    fix weight.py path

--- mkwfig.sh
+++ mkwfig.sh
@@ -2,8 +2,8 @@
 
 A=/var/www/html/weight.png
 B=/home/pi/share/統計記録.xls
-CMD=/home/pi/weight.py
-if [ $A -e ]; then
+CMD=/home/pi/weight-transition/weight.py
+if [ -e $A ]; then
 	if [ $A -ot $B ]; then
 		python $CMD
 	fi
