57a335fc9ddaddbbf8e15e92ce167d5236c32a31
p06044 <p06044@gmail.com>
2018-01-28 23:19:21 +0900

    update

--- urlencode.sh
+++ urlencode.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo `ruby -r cgi -e "puts CGI.escape(\""$1"\")"`
+if [ $1 = "encode" ]; then
+	echo `ruby -r cgi -e "puts CGI.escape(\""$2"\")"`
+elif [ $1 = "decode" ]; then
+	echo `ruby -r cgi -e "puts CGI.unescape(\""$2"\")"`
+fi
