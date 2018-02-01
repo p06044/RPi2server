d08041c5fa1370c501ab9a89479cd2f191b37f30
p06044 <p06044@gmail.com>
2018-02-02 08:52:19 +0900

    apply cgi

--- tumblr.py
+++ tumblr.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-# coding: utf-8
-print "<html>"
+#coding: utf-8
+print "Content-type: text/html\n";
+print "<HTML>"
 print "<head>"
-print "	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" >"
-print "	<meta charset=\"UTF-8\" >"
+print "<meta charset=\"UTF-8\" >"
+print "<title>tumblr tag</title>"
 print "</head>"
 print "<body>"
 print "<a href=\"amazonlist.html\">[ama]</a><a href=\"cron.html\">[cron]</a><a href=\"auctionlist.html\">[meru]</a><a href=\"ya.html\">[yauc]</a><a href=\"timer.html\">[timer]</a><a href=\"phpbutton.html\">[button]</a><span style=\"color:orange;\">[tumblr]</span>"
@@ -15,6 +16,11 @@ for line in areas:
 	print "<a href=\"http://sortme.tumblr.com/tagged/"+line.split()[0]+"\" target=\"_blank\">"+line.split()[1]+"</a>"
 f.close()
 
+f = open('tumblrG.txt')
+areas = f.read().splitlines()
+for line in areas:
+	print "<a href=\"http://gravua.tumblr.com/tagged/"+line.split()[0]+"\" target=\"_blank\">"+line.split()[1]+"</a>"
+f.close()
 
 print "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"
 print "<!-- ふゎーおすまほ -->"
@@ -25,4 +31,7 @@ print "     data-ad-slot=\"2135626673\"></ins>"
 print "<script>"
 print "(adsbygoogle = window.adsbygoogle || []).push({});"
 print "</script>"
+print ""
 print "</body>"
+print ""
+print "</HTML>"
