89b7545c2c08bd967f617d3723ec434fd5c19c8e
p06044 <p06044@gmail.com>
2018-02-02 01:23:53 +0900

    add tumblr.py

---
+++ tumblr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# coding: utf-8
+print "<html>"
+print "<head>"
+print "	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" >"
+print "	<meta charset=\"UTF-8\" >"
+print "</head>"
+print "<body>"
+print "<a href=\"amazonlist.html\">[ama]</a><a href=\"cron.html\">[cron]</a><a href=\"auctionlist.html\">[meru]</a><a href=\"ya.html\">[yauc]</a><a href=\"timer.html\">[timer]</a><a href=\"phpbutton.html\">[button]</a><span style=\"color:orange;\">[tumblr]</span>"
+print "<br/>"
+
+f = open('tumblrS.txt')
+areas = f.read().splitlines()
+for line in areas:
+	print "<a href=\"http://sortme.tumblr.com/tagged/"+line.split()[0]+"\" target=\"_blank\">"+line.split()[1]+"</a>"
+f.close()
+
+
+print "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>"
+print "<!-- ふゎーおすまほ -->"
+print "<ins class=\"adsbygoogle\""
+print "     style=\"display:inline-block;width:300px;height:250px\""
+print "     data-ad-client=\"ca-pub-8948717586645505\""
+print "     data-ad-slot=\"2135626673\"></ins>"
+print "<script>"
+print "(adsbygoogle = window.adsbygoogle || []).push({});"
+print "</script>"
+print "</body>"
