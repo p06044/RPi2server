aedadcc383fb43e474c04e2135a1f52165e21201
p06044 <p06044@gmail.com>
2018-02-07 10:21:31 +0900

    update

--- bs.py
+++ bs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#coding:utf-8
 import requests
 from bs4 import BeautifulSoup
 url = "https://auctions.yahoo.co.jp/search/search?p=kals+%E8%A6%81%E9%A0%85%E9%9B%86"
@@ -6,5 +7,20 @@ html = requests.get(url)
 soup = BeautifulSoup(html.text, "html5lib")
 
 #print soup.prettify()
-print soup.find(class_="pr1")
-print soup.find(class_="a1wrp")
+title = []
+for tag in soup.find_all("h3"):
+	title.append(tag.get_text())
+
+price = []
+for tag in soup.find_all(class_="pr1"):
+	tag.ul.extract()
+	text = tag.get_text().replace("\n","").replace(" ","")
+	price.append(text)
+
+link = []
+for tag in soup.find_all("h3"):
+	tag.find("h3")
+	print tag
+
+#for line in range(len(title)):
+#	print('%s, %s' % (title[line], price[line]))
