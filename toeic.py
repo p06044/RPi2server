91d85af96b8a27fc046a4e5d7106dc665cb7be4c
p06044 <p06044@gmail.com>
2018-02-14 07:52:40 +0900

    fix outfile, bug

--- toeic.py
+++ toeic.py
@@ -2,7 +2,12 @@
 #coding:utf-8
 import requests
 import sys
+import datetime
+
+date = datetime.date.today()
+outf = '/home/pi/share/'+str(date)+'.html'
+print outf
 url = 'https://www.iibc-global.org/toeic/test/lr/about/pr.html'
 res = requests.get(url)
-with open('/home/pi/share/toeic.html, 'w') as file:
+with open(outf, 'w') as file:
     file.write(res.text.encode('utf-8'))
