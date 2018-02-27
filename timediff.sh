db8f92ba5de4522b38957be5c977cc218f24b603
p06044 <p06044@gmail.com>
2018-02-27 18:30:33 +0900

    update

--- timediff.sh
+++ timediff.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 nowD=`date "+%s"`
-birthD=`date -d "20180225" '+%s'`
+birthD=`date -d "20180610" '+%s'`
 ret=`expr "$birthD" - "$nowD"`
 ret=`expr $ret / 86400 + 1`
-echo "<div>二次試験まで<span style="color:blue">$ret</span>日！</div>"
-#echo "<div>二次試験まで<span style="color:blue">$ret</span>日！</div>" >> /home/pi/cron.html
+echo "<div>簿記まで<span style="color:blue">$ret</span>日！</div>"
