2d045da37bc5d85250b669da15308119bfc9ebc8
p06044 <p06044@gmail.com>
2018-02-27 18:35:06 +0900

    update

--- timediff.sh
+++ timediff.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 nowD=`date "+%s"`
+#簿記
 birthD=`date -d "20180610" '+%s'`
 ret=`expr "$birthD" - "$nowD"`
 ret=`expr $ret / 86400 + 1`
 echo "<div>簿記まで<span style="color:blue">$ret</span>日！</div>"
+#宅建
+birthD=`date -d "20181021" '+%s'`
+ret=`expr "$birthD" - "$nowD"`
+ret=`expr $ret / 86400 + 1`
+echo "<div>宅建まで<span style="color:blue">$ret</span>日！</div>"
