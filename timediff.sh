6d6b175f16a0c2ee5c0b4da4f97ce086eee67202
p06044 <p06044@gmail.com>
2018-03-01 08:40:58 +0900

    update

--- timediff.sh
+++ timediff.sh
@@ -14,3 +14,7 @@ birthD=`date -d "20190204" '+%s'`
 ret=`expr "$birthD" - "$nowD"`
 ret=`expr $ret / 86400 + 1`
 echo "<div>社福まで<span style="color:blue">$ret</span>日！</div>"
+birthD=`date -d "20190119" '+%s'`
+ret=`expr "$birthD" - "$nowD"`
+ret=`expr $ret / 86400 + 1`
+echo "<div>センターまで<span style="color:blue">$ret</span>日！</div>"
