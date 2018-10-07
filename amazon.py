ed6a1802874da1afa86e83849ff8357ad9c633b5
p06044 <p06044@gmail.com>
2018-10-08 00:13:09 +0900

    fix "

--- amazon.py
+++ amazon.py
@@ -9,7 +9,7 @@ date = datetime.date.today()
 print date 
 cgifunc.afi()
 print "<br/>"
-print "<a href="https://www.amazon.co.jp/hz/wishlist/ls/3DWA0M1KJI44Z?&sort=default" target="_blank">ほしい物リスト</a><br/>"
+print "<a href=\"https://www.amazon.co.jp/hz/wishlist/ls/3DWA0M1KJI44Z?&sort=default\" target=\"_blank\">ほしい物リスト</a><br/>"
 f = open('/home/pi/gittxt/amazonhtml.txt')
 areas = f.read().splitlines()
 for line in areas:
