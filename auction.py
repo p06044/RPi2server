42bb5051925e53e2fc57692259ba442da321fde6
p06044 <p06044@gmail.com>
2018-10-08 00:10:52 +0900

    fix target blank

--- auction.py
+++ auction.py
@@ -9,6 +9,7 @@ date = datetime.date.today()
 print date 
 cgifunc.afi()
 print "<br/>"
+print "<a href="http://fuwaao.blogspot.com/search/label/%E3%83%A1%E3%83%AB%E3%82%AB%E3%83%AA%E5%87%BA%E5%93%81%E4%B8%80%E8%A6%A7" target="_blank">出品リスト</a><br/>"
 
 f = open('auction.txt')
 areas = f.read().splitlines()
