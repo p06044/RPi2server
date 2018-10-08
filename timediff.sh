41dd065d2892eff1b1333403c6abd2061b19597c
p06044 <p06044@gmail.com>
2018-10-08 21:50:29 +0900

    change timediff.sh timediff.txt

--- timediff.sh
+++ timediff.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 nowD=`date "+%s"`
-#birthD=`date -d "20180826" '+%s'`
-#ret=`expr "$birthD" - "$nowD"`
-#ret=`expr $ret / 86400 + 1`
-#echo "<div>社労士まで<span style="color:blue">$ret</span>日！</div>"
-birthD=`date -d "20181007" '+%s'`
-ret=`expr "$birthD" - "$nowD"`
-ret=`expr $ret / 86400 + 1`
-echo "<div>電工まで<span style="color:blue">$ret</span>日！</div>"
-birthD=`date -d "20181020" '+%s'`
-ret=`expr "$birthD" - "$nowD"`
-ret=`expr $ret / 86400 + 1`
-echo "<div>ボイラーまで<span style="color:blue">$ret</span>日！</div>"
-birthD=`date -d "20181111" '+%s'`
-ret=`expr "$birthD" - "$nowD"`
-ret=`expr $ret / 86400 + 1`
-echo "<div>行書まで<span style="color:blue">$ret</span>日！</div>"
-birthD=`date -d "20181208" '+%s'`
-ret=`expr "$birthD" - "$nowD"`
-ret=`expr $ret / 86400 + 1`
-echo "<div>電工実技まで<span style="color:blue">$ret</span>日！</div>"
-birthD=`date -d "20190119" '+%s'`
-ret=`expr "$birthD" - "$nowD"`
-ret=`expr $ret / 86400 + 1`
-echo "<div>センターまで<span style="color:blue">$ret</span>日！</div>"
-birthD=`date -d "20190204" '+%s'`
-ret=`expr "$birthD" - "$nowD"`
-ret=`expr $ret / 86400 + 1`
-echo "<div>社福まで<span style="color:blue">$ret</span>日！</div>"
+calc () {
+	birthD=`date -d $1 '+%s'`
+	ret=`expr "$birthD" - "$nowD"`
+	ret=`expr $ret / 86400 + 1`
+	echo "<div>$2まで<span style="color:blue">$ret</span>日！</div>"
+}
 
+cat timediff.txt | while read line
+do
+	calc `echo $line | awk '{print $1}'` `echo $line | awk '{print $2}'`
+done
